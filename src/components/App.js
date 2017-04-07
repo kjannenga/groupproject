8462e08a5102d2d21165927a5bdd93eb4d2ecc06
KrystineJannenga <kjannenga1@gmail.com>
2017-04-07 13:00:17 -0700

    krystine's freakout

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,12 @@
 import React from 'react'
 import {BrowserRouter as Router, Route} from 'react-router-dom'
-<<<<<<< HEAD
-import Story from './Story'
+import Story from './story'
 import Reservations from './Reservations'
-import Menu from './Menu'
-import News from './News'
+import Menu from './menu'
+import News from './news'
 import './main.css'
-import Special from './Special'
-=======
-import story from './story'
-import reservation from './reservation'
-import menu from './menu'
-import news from './news'
-import special from './special'
-import main from "./main.css"
+import Special from './special'
 import Logo from './images/eatmygrass.jpg'
-
-
-
-
-styles:{
-
-}
-
-
 
 
 export default React.createClass({
@@ -32,18 +15,7 @@ export default React.createClass({
     return (
       	<Router>
 			<div>
-		      	<div className="header"> 
-					<img className="logo" alt="logo" src="https://files.slack.com/files-pri/T03FAV5N3-F4V4QHEF4/eatmygrass.jpg" width="600" height="150" /> 
-					<div className="navBar">
-						<ul>
-							<li><a href="/">Story</a></li>
-							<li><a href="#menu">Menu</a></li>
-							<li><a href="#reservation">Reservations</a></li>
-							<li><a href="#news">News</a></li>
-							<li><a href="#specialDiv">Special</a></li>
-						</ul>
-
-
+		      	
 		      <div className="header"> 
 				<img className="logo" alt="logo" src={Logo} /> 
 			
@@ -65,37 +37,29 @@ export default React.createClass({
 
 
 
-
-
-
-
-
-
-
-
-
-
-
 			<div className="pages">
 			
 
-		      <Route exact={true} path="/" component={story} />
-		      <Route path="/menu/" component={menu} />
-		      <Route path="/reservation/" component={reservation} />
-		      <Route path="/news/" component={news} />
-		      <Route path="/special/" component={special} />
+		      <Route exact={true} path="/" component={Story} />
+		      <Route path="/menu/" component={Menu} />
+		      <Route path="/reservations/" component={Reservations} />
+		      <Route path="/news/" component={News} />
+		      <Route path="/special/" component={Special} />
 
 		    </div>
+
+		    </div>
+		    
 		    
 		        
 		        
 		     
 		      
-			</div>
+		
 			
     </Router>
      
     )
   }
->>>>>>> krystine
+
 })
